5c7c5e640844040f5cee0ca91b751e8d20f1cab3
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-10-22 14:27:08 +0200

    added a wat to change the name of the property in the view and added range to give an int a minimum or maximum value

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Razor.Language.Extensions;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
@@ -12,6 +13,11 @@ namespace BulkyBookWeb.Models
         // data annotation for a requirment 
         [Required]
         public string Name { get; set; }
+        // DisplayName attribute is needed to display a porpety that is not the same as the property name
+        [DisplayName("Display Order")]
+        // Range attribute is used for choosing a range between min num and max num
+        // The ErrorMessage is to give a custom error message by choice
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
 
         // This way you can give default time the datetime now
